ce662cd4cc4477b2e7e23d55f0c39403b39de8db
Eduardo Dalhke Lopes <dalhkedu@gmail.com>
2022-03-01 13:51:58 -0300

    inicializando javascript

--- functions.php
+++ functions.php
@@ -34,7 +34,7 @@ function lam_register_scripts() {
     wp_enqueue_script( 'lam-js', get_template_directory_uri() . '/assets/js/materialize.min.js', array(), $theme_version, true );
 	#wp_script_add_data( 'lam-js', 'async', true );
 
-    wp_enqueue_script( 'lam-event', get_template_directory_uri() . '/assets/js/materialize.min.js', array('lam-js'), $theme_version, true );
+    wp_enqueue_script( 'lam-event', get_template_directory_uri() . '/assets/js/events.js', array('lam-js'), $theme_version, true );
 	#wp_script_add_data( 'lam-event', 'async', true );
 
 }
