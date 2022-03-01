119e5b41065a9b878f6a560c7d64ec30b7085ee8
Eduardo Dalhke Lopes <dalhkedu@gmail.com>
2022-03-01 13:51:12 -0300

    inicializando javascript

--- functions.php
+++ functions.php
@@ -27,17 +27,15 @@ function lam_register_scripts() {
 
 	$theme_version = wp_get_theme()->get( 'Version' );
 
-    if ( !is_admin() ) wp_deregister_script('jquery');
-
 	if ( (!is_admin() ) && is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
 
-    wp_enqueue_script( 'lam-js', get_template_directory_uri() . '/assets/js/materialize.min.js', array(), $theme_version, false );
-	wp_script_add_data( 'lam-js', 'async', true );
+    wp_enqueue_script( 'lam-js', get_template_directory_uri() . '/assets/js/materialize.min.js', array(), $theme_version, true );
+	#wp_script_add_data( 'lam-js', 'async', true );
 
-    wp_enqueue_script( 'lam-event', get_template_directory_uri() . '/assets/js/materialize.min.js', array('lam-js'), $theme_version, false );
-	wp_script_add_data( 'lam-event', 'async', true );
+    wp_enqueue_script( 'lam-event', get_template_directory_uri() . '/assets/js/materialize.min.js', array('lam-js'), $theme_version, true );
+	#wp_script_add_data( 'lam-event', 'async', true );
 
 }
 add_action( 'wp_enqueue_scripts', 'lam_register_scripts' );
