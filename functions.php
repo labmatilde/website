41d3697e9d7a30b2009c207e44fc96ed2194e62a
Eduardo Dalhke Lopes <dalhkedu@gmail.com>
2022-03-01 13:11:54 -0300

    montando menu

--- functions.php
+++ functions.php
@@ -32,6 +32,7 @@ function lam_register_scripts() {
 	}
 
 	wp_enqueue_script( 'lam-js', get_template_directory_uri() . '/assets/js/materialize.min.js', array(), $theme_version, false );
+    wp_enqueue_script( 'lam-js', get_template_directory_uri() . '/assets/js/events.js', array(), $theme_version, false );
 	wp_script_add_data( 'lam-js', 'async', true );
 
 }
