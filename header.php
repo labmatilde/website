ea4f618bf78fa66a9ab967cccd5d142800fa4ff8
Eduardo Dalhke Lopes <dalhkedu@gmail.com>
2022-03-01 12:34:50 -0300

    montando menu

--- header.php
+++ header.php
@@ -13,13 +13,6 @@
 $args = array(
   'menu_class'        => "left hide-on-med-and-down", // (string) CSS class to use for the ul element which forms the menu. Default 'menu'.
   'menu_id'           => "nav-mobile", // (string) The ID that is applied to the ul element which forms the menu. Default is the menu slug, incremented.
-  'container'         => "lam_container", // (string) Whether to wrap the ul, and what to wrap it with. Default 'div'.
-  'container_class'   => "lam_container_class", // (string) Class that is applied to the container. Default 'menu-{menu slug}-container'.
-  'container_id'      => "lam_container_id", // (string) The ID that is applied to the container.
-  'before'            => "texto que vem antes", // (string) Text before the link markup.
-  'after'             => "texto que vem depois", // (string) Text after the link markup.
-  'link_before'       => "link que vem antes", // (string) Text before the link text.
-  'link_after'        => "link que vem depois", // (string) Text after the link text.
   'echo'              => "true", // (bool) Whether to echo the menu or return it. Default true.
   'depth'             => "0", // (int) How many levels of the hierarchy are to be included. 0 means all. Default 0.
   'theme_location'    => "primary_menu", // (string) Theme location to be used. Must be registered with register_nav_menu() in order to be selectable by the user.
@@ -44,10 +37,10 @@ $args = array(
 <?php wp_body_open(); ?>
 <body <?php body_class(); ?>>
 
-    <nav>
-
+  <nav>
+    <div class="nav-wrapper">
     <?php wp_nav_menu( $args ); ?>
-
+    </div>
   </nav>
   <?php 
   
