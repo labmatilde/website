4c0420a6afce158d4dc9147e44907743a226e449
Eduardo Dalhke Lopes <dalhkedu@gmail.com>
2022-03-01 15:56:00 -0300

    add carrousel

--- assets/js/events.js
+++ assets/js/events.js
@@ -1,3 +1,12 @@
 'use strict';
 
 M.AutoInit();
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.carousel');
+    var instances = M.Carousel.init(elems, {
+        fullWidth: true,
+        indicators: true
+    });
+});
