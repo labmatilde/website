09fc5061ced62858bf71912493eca55dc46562c1
Eduardo Dalhke Lopes <dalhkedu@gmail.com>
2022-03-01 19:01:03 -0300

    novo layout

--- header.php
+++ header.php
@@ -58,22 +58,10 @@ $args_mobile = array(
   <div class="nav-wrapper">
     <a href="horizontal-half.html" class="brand-logo"><i class="icon-diamond white-text"></i></a>
     <ul id="nav-mobile" class="right hide-on-med-and-down">
-      <li class="active"><a class="dropdown-button" href="#!" data-activates="pages" data-constrainWidth="false" data-belowOrigin="true">Pages<i class="material-icons right">arrow_drop_down</i></a></li>
-      <li><a href="blog.html">Blog</a></li>
-      <li><a href="team.html">Team</a></li>
-      <li><a href="docs/about.html">Docs</a></li>
-      <li><a href="#">Buy Now!</a></li>
-    </ul>
-
-    <ul id="pages" class="dropdown-content">
-      <li><a class="active" href="horizontal-half.html">Horizontal Halves</a></li>
-      <li><a href="sierra.html">Zoom Out</a></li>
-      <li><a href="circle-reveal.html">Circle Reveal</a></li>
-      <li><a href="phone-wall.html">Phone Wall</a></li>
-      <li><a href="element-transitions.html">Element Transitions</a></li>
-      <li><a href="basic-elements.html">Basic Elements</a></li>
-      <li><a href="card-shuffle.html">Shuffle</a></li>
-      <li><a href="postcards.html">Postcards</a></li>
+      <li><a href="#">Inicio</a></li>
+      <li><a href="#">Blog</a></li>
+      <li><a href="#">Time</a></li>
+      <li><a href="#">Sobre</a></li>
     </ul>
 
     <a href="#" data-activates="slide-out" class="button-collapse right"><i class="material-icons white-text">menu</i></a>
@@ -81,29 +69,10 @@ $args_mobile = array(
 </nav>
 
 <ul id="slide-out" class="side-nav">
-    <li class="no-padding">
-        <ul class="collapsible collapsible-accordion">
-            <li class="bold">
-            <a class="collapsible-header waves-effect waves-teal active">Pages</a>
-            <div class="collapsible-body">
-                <ul>
-                    <li><a class="active" href="horizontal-half.html">Horizontal Halves</a></li>
-                    <li><a href="sierra.html">Zoom Out</a></li>
-                    <li><a href="circle-reveal.html">Circle Reveal</a></li>
-                    <li><a href="phone-wall.html">Phone Wall</a></li>
-                    <li><a href="element-transitions.html">Element Transitions</a></li>
-                    <li><a href="basic-elements.html">Basic Elements</a></li>
-                    <li><a href="card-shuffle.html">Shuffle</a></li>
-                    <li><a href="postcards.html">Postcards</a></li>
-                </ul>
-            </div>
-            </li>
-        </ul>
-    </li>
-    <li><a class="waves-effect waves-teal" href="blog.html">Blog</a></li>
-    <li><a class="waves-effect waves-teal" href="team.html">Team</a></li>
-    <li><a class="waves-effect waves-teal" href="docs/about.html">Docs</a></li>
-    <li><a class="waves-effect waves-teal" href="#">Buy Now!</a></li>
+    <li><a class="waves-effect waves-teal" href="#">Inicio</a></li>
+    <li><a class="waves-effect waves-teal" href="#">Blog</a></li>
+    <li><a class="waves-effect waves-teal" href="#">Time</a></li>
+    <li><a class="waves-effect waves-teal" href="#">Sobre</a></li>
 </ul>
 
 
