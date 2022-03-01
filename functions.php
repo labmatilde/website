8696d1733d6320df03e37005dd821ae9d77d0914
Eduardo Dalhke Lopes <dalhkedu@gmail.com>
2022-03-01 13:43:47 -0300

    inicializando javascript

--- functions.php
+++ functions.php
@@ -33,8 +33,11 @@ function lam_register_scripts() {
 		wp_enqueue_script( 'comment-reply' );
 	}
 
-	wp_enqueue_script( 'lam-js', get_template_directory_uri() . '/assets/js/materialize.min.js', array(), true );
-    wp_enqueue_script( 'lam-event', get_template_directory_uri() . '/assets/js/events.js', array('lam-js'), true );
+    wp_enqueue_script( 'lam-js', get_template_directory_uri() . '/assets/js/materialize.min.js', array(), $theme_version, false );
+	wp_script_add_data( 'lam-js', 'async', true );
+
+	#wp_enqueue_script( 'lam-js', get_template_directory_uri() . '/assets/js/materialize.min.js', array(), true );
+    #wp_enqueue_script( 'lam-event', get_template_directory_uri() . '/assets/js/events.js', array('lam-js'), true );
 
 }
 add_action( 'wp_enqueue_scripts', 'lam_register_scripts' );
