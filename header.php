e7744a7055724804bb304b930def776550308016
Eduardo Dalhke Lopes <dalhkedu@gmail.com>
2022-03-01 12:16:58 -0300

    montando menu

--- header.php
+++ header.php
@@ -51,19 +51,11 @@ $args = array(
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
     <nav>
-    <div class="nav-wrapper">
-      <a href="#" class="brand-logo right">Logo</a>
-      <ul id="nav-mobile" class="left hide-on-med-and-down">
-        <li><a href="sass.html">Sass</a></li>
-        <li><a href="badges.html">Components</a></li>
-        <li><a href="collapsible.html">JavaScript</a></li>
-      </ul>
-    </div>
-  </nav>
-  <?php
 
-  wp_nav_menu( $args );
-  get_search_form();
+    <?php wp_nav_menu( $args ); ?>
+
+  </nav>
+  <?php get_search_form();
 
 
 
