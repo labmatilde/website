630050768dc950318db846df9388cf2a40ca09d1
Eduardo Dalhke Lopes <dalhkedu@gmail.com>
2022-04-11 21:36:11 -0300

    registrando custom post type de projetos

--- functions.php
+++ functions.php
@@ -1,1 +1,17 @@
 <?php
+
+function project_custom_post_type() {
+    register_post_type(
+        'lam_project',
+            array(
+                'labels'            => array(
+                'name'          => __('Projects', 'lam'),
+                'singular_name' => __('Project', 'lam'),
+            ),
+        'public'      => true,
+        'has_archive' => true,
+        'rewrite'     => array( 'slug' => 'projetos' )
+        )
+    );
+}
+add_action('init', 'project_custom_post_type');
