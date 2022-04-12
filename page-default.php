d440c017ac48f4838aae9f1504194b23d999129c
Eduardo Dalhke Lopes <dalhkedu@gmail.com>
2022-04-11 23:15:37 -0300

    apresentando projetos

--- page-default.php
+++ page-default.php
@@ -68,14 +68,19 @@
 </section> <!-- #sobre -->
 <?php endif; ?>
 
+
 <?php if(get_field('mostar_projetos')): ?>
 <!-- PROJETOS -->
 <section id="projetos" class="dark">
     <div class="container">
         <div class="row">
             <div class="col s12">
-                <h2 class="title">Projetos</h2>
+                <h2 class="title"><?php _e('Projetos'); ?></h2>
             </div>
+
+
+            <?php $values = get_field('selecionar_projetos_apresentados'); print_r($values); ?>
+
             <div class="col s12 m4">
                 <div class="card sticky-action hoverable">
                     <div class="card-image waves-effect waves-block waves-light">
@@ -98,203 +103,7 @@
                 </div> <!-- card -->
             </div> <!-- col -->
 
-            <div class="col s12 m4">
-                <div class="card sticky-action hoverable">
-                    <div class="card-image waves-effect waves-block waves-light">
-                        <img class="activator" src="<?= get_template_directory_uri(); ?>/assets/images/cartaz-enxoval.png">
-                    </div> <!-- card-image -->
-                    <div class="card-content">
-                        <span class="card-title activator grey-text text-darken-4">Gestantes<i
-                                class="material-icons right">more_vert</i></span>
-                        <p><a href="projetos.html">Acessar projeto</a></p>
-                    </div> <!-- card-content -->
-                    <div class="card-reveal">
-                        <span class="card-title grey-text text-darken-4">Projeto Gestantes<i
-                                class="material-icons right">close</i></span>
-                        <p>Here is some more information about this product that is only revealed once clicked on.
-                        </p>
-                        <p class="card-pix"><i class="material-icons">account_balance</i><span
-                                class="content-card-pix">CHAVE
-                                PIX: 40.416.297/0001-95</span></p>
-                    </div> <!-- card-reveal -->
-                </div> <!-- card -->
-            </div> <!-- col -->
-
-            <div class="col s12 m4">
-                <div class="card sticky-action hoverable">
-                    <div class="card-image waves-effect waves-block waves-light">
-                        <img class="activator" src="<?= get_template_directory_uri(); ?>/assets/images/cartaz-samaritanos.png">
-                    </div> <!-- card-image -->
-                    <div class="card-content">
-                        <span class="card-title activator grey-text text-darken-4">Samaritanos<i
-                                class="material-icons right">more_vert</i></span>
-                        <p><a href="projetos.html">Acessar projeto</a></p>
-                    </div> <!-- card-content -->
-                    <div class="card-reveal">
-                        <span class="card-title grey-text text-darken-4">Projeto Samaritanos<i
-                                class="material-icons right">close</i></span>
-                        <p>Here is some more information about this product that is only revealed once clicked on.
-                        </p>
-                        <p class="card-pix"><i class="material-icons">account_balance</i><span
-                                class="content-card-pix">CHAVE
-                                PIX: 40.416.297/0001-95</span></p>
-                    </div> <!-- card-reveal -->
-                </div> <!-- card -->
-            </div> <!-- col -->
-
-            <div class="col s12 m4">
-                <div class="card sticky-action hoverable">
-                    <div class="card-image waves-effect waves-block waves-light">
-                        <img class="activator" src="<?= get_template_directory_uri(); ?>/assets/images/cartaz-samaritanos.png">
-                    </div> <!-- card-image -->
-                    <div class="card-content">
-                        <span class="card-title activator grey-text text-darken-4">Resgatando Almas<i
-                                class="material-icons right">more_vert</i></span>
-                        <p><a href="projetos.html">Acessar projeto</a></p>
-                    </div> <!-- card-content -->
-                    <div class="card-reveal">
-                        <span class="card-title grey-text text-darken-4">Projeto Resgatando Almas<i
-                                class="material-icons right">close</i></span>
-                        <p>Here is some more information about this product that is only revealed once clicked on.
-                        </p>
-                        <p class="card-pix"><i class="material-icons">account_balance</i><span
-                                class="content-card-pix">CHAVE
-                                PIX: 40.416.297/0001-95</span></p>
-                    </div> <!-- card-reveal -->
-                </div> <!-- card -->
-            </div> <!-- col -->
-
-            <div class="col s12 m4">
-                <div class="card sticky-action hoverable">
-                    <div class="card-image waves-effect waves-block waves-light">
-                        <img class="activator" src="<?= get_template_directory_uri(); ?>/assets/images/cartaz-samaritanos.png">
-                    </div> <!-- card-image -->
-                    <div class="card-content">
-                        <span class="card-title activator grey-text text-darken-4">Ame se<i
-                                class="material-icons right">more_vert</i></span>
-                        <p><a href="projetos.html">Acessar projeto</a></p>
-                    </div> <!-- card-content -->
-                    <div class="card-reveal">
-                        <span class="card-title grey-text text-darken-4">Projeto Ame se<i
-                                class="material-icons right">close</i></span>
-                        <p>Here is some more information about this product that is only revealed once clicked on.
-                        </p>
-                        <p class="card-pix"><i class="material-icons">account_balance</i><span
-                                class="content-card-pix">CHAVE
-                                PIX: 40.416.297/0001-95</span></p>
-                    </div> <!-- card-reveal -->
-                </div> <!-- card -->
-            </div> <!-- col -->
-
-            <div class="col s12 m4">
-                <div class="card sticky-action hoverable">
-                    <div class="card-image waves-effect waves-block waves-light">
-                        <img class="activator" src="<?= get_template_directory_uri(); ?>/assets/images/cartaz-samaritanos.png">
-                    </div> <!-- card-image -->
-                    <div class="card-content">
-                        <span class="card-title activator grey-text text-darken-4">Mocidade<i
-                                class="material-icons right">more_vert</i></span>
-                        <p><a href="projetos.html">Acessar projeto</a></p>
-                    </div> <!-- card-content -->
-                    <div class="card-reveal">
-                        <span class="card-title grey-text text-darken-4">Projeto Mocidade<i
-                                class="material-icons right">close</i></span>
-                        <p>Here is some more information about this product that is only revealed once clicked on.
-                        </p>
-                        <p class="card-pix"><i class="material-icons">account_balance</i><span
-                                class="content-card-pix">CHAVE
-                                PIX: 40.416.297/0001-95</span></p>
-                    </div> <!-- card-reveal -->
-                </div> <!-- card -->
-            </div> <!-- col -->
-
-            <div class="col s12 m4">
-                <div class="card sticky-action hoverable">
-                    <div class="card-image waves-effect waves-block waves-light">
-                        <img class="activator" src="<?= get_template_directory_uri(); ?>/assets/images/cartaz-samaritanos.png">
-                    </div> <!-- card-image -->
-                    <div class="card-content">
-                        <span class="card-title activator grey-text text-darken-4">Gaiolas Abertas<i
-                                class="material-icons right">more_vert</i></span>
-                        <p><a href="projetos.html">Acessar projeto</a></p>
-                    </div> <!-- card-content -->
-                    <div class="card-reveal">
-                        <span class="card-title grey-text text-darken-4">Projeto Gaiolas Abertas<i
-                                class="material-icons right">close</i></span>
-                        <p>Here is some more information about this product that is only revealed once clicked on.
-                        </p>
-                        <p class="card-pix"><i class="material-icons">account_balance</i><span
-                                class="content-card-pix">CHAVE
-                                PIX: 40.416.297/0001-95</span></p>
-                    </div> <!-- card-reveal -->
-                </div> <!-- card -->
-            </div> <!-- col -->
-
-            <div class="col s12 m4">
-                <div class="card sticky-action hoverable">
-                    <div class="card-image waves-effect waves-block waves-light">
-                        <img class="activator" src="<?= get_template_directory_uri(); ?>/assets/images/cartaz-samaritanos.png">
-                    </div> <!-- card-image -->
-                    <div class="card-content">
-                        <span class="card-title activator grey-text text-darken-4">Clinica Vet<i
-                                class="material-icons right">more_vert</i></span>
-                        <p><a href="projetos.html">Acessar projeto</a></p>
-                    </div> <!-- card-content -->
-                    <div class="card-reveal">
-                        <span class="card-title grey-text text-darken-4">Projeto Clinica Vet<i
-                                class="material-icons right">close</i></span>
-                        <p>Here is some more information about this product that is only revealed once clicked on.
-                        </p>
-                        <p class="card-pix"><i class="material-icons">account_balance</i><span
-                                class="content-card-pix">CHAVE
-                                PIX: 40.416.297/0001-95</span></p>
-                    </div> <!-- card-reveal -->
-                </div> <!-- card -->
-            </div> <!-- col -->
-
-            <div class="col s12 m4">
-                <div class="card sticky-action hoverable">
-                    <div class="card-image waves-effect waves-block waves-light">
-                        <img class="activator" src="<?= get_template_directory_uri(); ?>/assets/images/cartaz-samaritanos.png">
-                    </div> <!-- card-image -->
-                    <div class="card-content">
-                        <span class="card-title activator grey-text text-darken-4">Lam Doce Lar<i
-                                class="material-icons right">more_vert</i></span>
-                        <p><a href="projetos.html">Acessar projeto</a></p>
-                    </div> <!-- card-content -->
-                    <div class="card-reveal">
-                        <span class="card-title grey-text text-darken-4">Projeto Lam Doce Lar<i
-                                class="material-icons right">close</i></span>
-                        <p>Here is some more information about this product that is only revealed once clicked on.
-                        </p>
-                        <p class="card-pix"><i class="material-icons">account_balance</i><span
-                                class="content-card-pix">CHAVE
-                                PIX: 40.416.297/0001-95</span></p>
-                    </div> <!-- card-reveal -->
-                </div> <!-- card -->
-            </div> <!-- col -->
-
-            <div class="col s12 m4">
-                <div class="card sticky-action hoverable">
-                    <div class="card-image waves-effect waves-block waves-light">
-                        <img class="activator" src="<?= get_template_directory_uri(); ?>/assets/images/cartaz-samaritanos.png">
-                    </div> <!-- card-image -->
-                    <div class="card-content">
-                        <span class="card-title activator grey-text text-darken-4">Irmão Francisco<i
-                                class="material-icons right">more_vert</i></span>
-                        <p><a href="projetos.html">Acessar projeto</a></p>
-                    </div> <!-- card-content -->
-                    <div class="card-reveal">
-                        <span class="card-title grey-text text-darken-4">Projeto Irmão Francisco<i
-                                class="material-icons right">close</i></span>
-                        <p>Here is some more information about this product that is only revealed once clicked on.
-                        </p>
-                        <p class="card-pix"><i class="material-icons">account_balance</i><span
-                                class="content-card-pix">CHAVE
-                                PIX: 40.416.297/0001-95</span></p>
-                    </div> <!-- card-reveal -->
-                </div> <!-- card -->
-            </div> <!-- col -->
+        
         </div> <!-- .row -->
     </div> <!-- .container -->
 </section> <!-- #projetos -->
