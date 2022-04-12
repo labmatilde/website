61861404bff1f20c109f8f3340556896b77902d5
Eduardo Dalhke Lopes <dalhkedu@gmail.com>
2022-04-11 22:16:36 -0300

    alterando nome arquivo single post type

---
+++ single-project_lam.php
@@ -0,0 +1,280 @@
+<?php
+/**
+* Template Name: Singlepage
+*
+* @package Labmatilde
+* @subpackage lam
+* @since Lar Assistencial Matilde 1.0
+*/
+?>
+<?php get_header();?>
+
+
+<?php while ( have_posts() ) : the_post(); ?>
+<div class="body">
+	<div class="container">
+		<div class="clear"></div>
+		<div class="main">
+			<div class="post content">
+				<h1 class="page-title"><a href="<?php the_permalink(); ?>"><?php the_title();?></a></h1>
+
+				<div class="content">
+					<?php the_content(); ?>
+				</div>
+			</div>
+		</div>
+		<div class="clear"></div>
+	</div>
+</div>
+<?php endwhile; ?>
+
+
+<!-- PROJETO -->
+<section id="projeto">
+	<div class="container">
+		<div class="row">
+			<div class="col s12 m8">
+				<h1 class="title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+				<div class="content">
+					<?php the_content(); ?>
+				</div>
+			</div>
+			<div class="sidebar col s12 m4">
+				<?php get_sidebar(); ?>
+			</div>
+		</div>
+	</div>
+</section>
+
+<!-- PROJETOS -->
+<section id="projetos" class="dark">
+	<div class="container">
+		<div class="row">
+			<div class="col s12">
+				<h2 class="title">Projetos</h2>
+			</div>
+			<div class="col s12 m4">
+				<div class="card sticky-action hoverable">
+					<div class="card-image waves-effect waves-block waves-light">
+						<img class="activator" src="./assets/images/cartaz-familia-lam.png">
+					</div> <!-- card-image -->
+					<div class="card-content">
+						<span class="card-title activator grey-text text-darken-4">Familias<i
+								class="material-icons right">more_vert</i></span>
+						<p><a href="projetos.html">Acessar projeto</a></p>
+					</div> <!-- card-content -->
+					<div class="card-reveal">
+						<span class="card-title grey-text text-darken-4">Projeto Familias<i
+								class="material-icons right">close</i></span>
+						<p>Here is some more information about this product that is only revealed once clicked on.
+						</p>
+						<p class="card-pix"><i class="material-icons">account_balance</i><span
+								class="content-card-pix">CHAVE
+								PIX: 40.416.297/0001-95</span></p>
+					</div> <!-- card-reveal -->
+				</div> <!-- card -->
+			</div> <!-- col -->
+
+			<div class="col s12 m4">
+				<div class="card sticky-action hoverable">
+					<div class="card-image waves-effect waves-block waves-light">
+						<img class="activator" src="./assets/images/cartaz-enxoval.png">
+					</div> <!-- card-image -->
+					<div class="card-content">
+						<span class="card-title activator grey-text text-darken-4">Gestantes<i
+								class="material-icons right">more_vert</i></span>
+						<p><a href="projetos.html">Acessar projeto</a></p>
+					</div> <!-- card-content -->
+					<div class="card-reveal">
+						<span class="card-title grey-text text-darken-4">Projeto Gestantes<i
+								class="material-icons right">close</i></span>
+						<p>Here is some more information about this product that is only revealed once clicked on.
+						</p>
+						<p class="card-pix"><i class="material-icons">account_balance</i><span
+								class="content-card-pix">CHAVE
+								PIX: 40.416.297/0001-95</span></p>
+					</div> <!-- card-reveal -->
+				</div> <!-- card -->
+			</div> <!-- col -->
+
+			<div class="col s12 m4">
+				<div class="card sticky-action hoverable">
+					<div class="card-image waves-effect waves-block waves-light">
+						<img class="activator" src="./assets/images/cartaz-samaritanos.png">
+					</div> <!-- card-image -->
+					<div class="card-content">
+						<span class="card-title activator grey-text text-darken-4">Samaritanos<i
+								class="material-icons right">more_vert</i></span>
+						<p><a href="projetos.html">Acessar projeto</a></p>
+					</div> <!-- card-content -->
+					<div class="card-reveal">
+						<span class="card-title grey-text text-darken-4">Projeto Samaritanos<i
+								class="material-icons right">close</i></span>
+						<p>Here is some more information about this product that is only revealed once clicked on.
+						</p>
+						<p class="card-pix"><i class="material-icons">account_balance</i><span
+								class="content-card-pix">CHAVE
+								PIX: 40.416.297/0001-95</span></p>
+					</div> <!-- card-reveal -->
+				</div> <!-- card -->
+			</div> <!-- col -->
+
+			<div class="col s12 m4">
+				<div class="card sticky-action hoverable">
+					<div class="card-image waves-effect waves-block waves-light">
+						<img class="activator" src="./assets/images/AME-SE.png">
+					</div> <!-- card-image -->
+					<div class="card-content">
+						<span class="card-title activator grey-text text-darken-4">Resgatando Almas<i
+								class="material-icons right">more_vert</i></span>
+						<p><a href="projetos.html">Acessar projeto</a></p>
+					</div> <!-- card-content -->
+					<div class="card-reveal">
+						<span class="card-title grey-text text-darken-4">Projeto Resgatando Almas<i
+								class="material-icons right">close</i></span>
+						<p>Here is some more information about this product that is only revealed once clicked on.
+						</p>
+						<p class="card-pix"><i class="material-icons">account_balance</i><span
+								class="content-card-pix">CHAVE
+								PIX: 40.416.297/0001-95</span></p>
+					</div> <!-- card-reveal -->
+				</div> <!-- card -->
+			</div> <!-- col -->
+
+			<div class="col s12 m4">
+				<div class="card sticky-action hoverable">
+					<div class="card-image waves-effect waves-block waves-light">
+						<img class="activator" src="./assets/images/AME-SE.png">
+					</div> <!-- card-image -->
+					<div class="card-content">
+						<span class="card-title activator grey-text text-darken-4">Ame se<i
+								class="material-icons right">more_vert</i></span>
+						<p><a href="projetos.html">Acessar projeto</a></p>
+					</div> <!-- card-content -->
+					<div class="card-reveal">
+						<span class="card-title grey-text text-darken-4">Projeto Ame se<i
+								class="material-icons right">close</i></span>
+						<p>Here is some more information about this product that is only revealed once clicked on.
+						</p>
+						<p class="card-pix"><i class="material-icons">account_balance</i><span
+								class="content-card-pix">CHAVE
+								PIX: 40.416.297/0001-95</span></p>
+					</div> <!-- card-reveal -->
+				</div> <!-- card -->
+			</div> <!-- col -->
+
+			<div class="col s12 m4">
+				<div class="card sticky-action hoverable">
+					<div class="card-image waves-effect waves-block waves-light">
+						<img class="activator" src="./assets/images/AME-SE.png">
+					</div> <!-- card-image -->
+					<div class="card-content">
+						<span class="card-title activator grey-text text-darken-4">Mocidade<i
+								class="material-icons right">more_vert</i></span>
+						<p><a href="projetos.html">Acessar projeto</a></p>
+					</div> <!-- card-content -->
+					<div class="card-reveal">
+						<span class="card-title grey-text text-darken-4">Projeto Mocidade<i
+								class="material-icons right">close</i></span>
+						<p>Here is some more information about this product that is only revealed once clicked on.
+						</p>
+						<p class="card-pix"><i class="material-icons">account_balance</i><span
+								class="content-card-pix">CHAVE
+								PIX: 40.416.297/0001-95</span></p>
+					</div> <!-- card-reveal -->
+				</div> <!-- card -->
+			</div> <!-- col -->
+
+			<div class="col s12 m4">
+				<div class="card sticky-action hoverable">
+					<div class="card-image waves-effect waves-block waves-light">
+						<img class="activator" src="./assets/images/AME-SE.png">
+					</div> <!-- card-image -->
+					<div class="card-content">
+						<span class="card-title activator grey-text text-darken-4">Gaiolas Abertas<i
+								class="material-icons right">more_vert</i></span>
+						<p><a href="projetos.html">Acessar projeto</a></p>
+					</div> <!-- card-content -->
+					<div class="card-reveal">
+						<span class="card-title grey-text text-darken-4">Projeto Gaiolas Abertas<i
+								class="material-icons right">close</i></span>
+						<p>Here is some more information about this product that is only revealed once clicked on.
+						</p>
+						<p class="card-pix"><i class="material-icons">account_balance</i><span
+								class="content-card-pix">CHAVE
+								PIX: 40.416.297/0001-95</span></p>
+					</div> <!-- card-reveal -->
+				</div> <!-- card -->
+			</div> <!-- col -->
+
+			<div class="col s12 m4">
+				<div class="card sticky-action hoverable">
+					<div class="card-image waves-effect waves-block waves-light">
+						<img class="activator" src="./assets/images/AME-SE.png">
+					</div> <!-- card-image -->
+					<div class="card-content">
+						<span class="card-title activator grey-text text-darken-4">Clinica Vet<i
+								class="material-icons right">more_vert</i></span>
+						<p><a href="projetos.html">Acessar projeto</a></p>
+					</div> <!-- card-content -->
+					<div class="card-reveal">
+						<span class="card-title grey-text text-darken-4">Projeto Clinica Vet<i
+								class="material-icons right">close</i></span>
+						<p>Here is some more information about this product that is only revealed once clicked on.
+						</p>
+						<p class="card-pix"><i class="material-icons">account_balance</i><span
+								class="content-card-pix">CHAVE
+								PIX: 40.416.297/0001-95</span></p>
+					</div> <!-- card-reveal -->
+				</div> <!-- card -->
+			</div> <!-- col -->
+
+			<div class="col s12 m4">
+				<div class="card sticky-action hoverable">
+					<div class="card-image waves-effect waves-block waves-light">
+						<img class="activator" src="./assets/images/AME-SE.png">
+					</div> <!-- card-image -->
+					<div class="card-content">
+						<span class="card-title activator grey-text text-darken-4">Lam Doce Lar<i
+								class="material-icons right">more_vert</i></span>
+						<p><a href="projetos.html">Acessar projeto</a></p>
+					</div> <!-- card-content -->
+					<div class="card-reveal">
+						<span class="card-title grey-text text-darken-4">Projeto Lam Doce Lar<i
+								class="material-icons right">close</i></span>
+						<p>Here is some more information about this product that is only revealed once clicked on.
+						</p>
+						<p class="card-pix"><i class="material-icons">account_balance</i><span
+								class="content-card-pix">CHAVE
+								PIX: 40.416.297/0001-95</span></p>
+					</div> <!-- card-reveal -->
+				</div> <!-- card -->
+			</div> <!-- col -->
+
+			<div class="col s12 m4">
+				<div class="card sticky-action hoverable">
+					<div class="card-image waves-effect waves-block waves-light">
+						<img class="activator" src="./assets/images/AME-SE.png">
+					</div> <!-- card-image -->
+					<div class="card-content">
+						<span class="card-title activator grey-text text-darken-4">Irmão Francisco<i
+								class="material-icons right">more_vert</i></span>
+						<p><a href="projetos.html">Acessar projeto</a></p>
+					</div> <!-- card-content -->
+					<div class="card-reveal">
+						<span class="card-title grey-text text-darken-4">Projeto Irmão Francisco<i
+								class="material-icons right">close</i></span>
+						<p>Here is some more information about this product that is only revealed once clicked on.
+						</p>
+						<p class="card-pix"><i class="material-icons">account_balance</i><span
+								class="content-card-pix">CHAVE
+								PIX: 40.416.297/0001-95</span></p>
+					</div> <!-- card-reveal -->
+				</div> <!-- card -->
+			</div> <!-- col -->
+		</div> <!-- .row -->
+	</div> <!-- .container -->
+</section> <!-- #projetos -->
+
+
+<?php get_footer();
