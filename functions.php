20742534c15cabb16871ab798cddfea0bba0eac6
Eduardo Dalhke Lopes <dalhkedu@gmail.com>
2022-08-02 15:45:23 -0300

    removendo warning

--- functions.php
+++ functions.php
@@ -322,4 +322,4 @@ function lam_add_woocommerce_support() {
 
 add_action( 'after_setup_theme', 'lam_add_woocommerce_support' );
 
-add_action ( 'action_tag' , array( $this , 'my_callback , 30 ) );
+add_action ( 'action_tag' , array( $this , 'my_callback' , 30 ) );
