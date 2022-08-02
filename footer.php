29524bcb637d152d06ce37e462ffc9ad3030aa33
Eduardo Dalhke Lopes <dalhkedu@gmail.com>
2022-08-02 17:27:31 -0300

    footer para doacoes

--- footer.php
+++ footer.php
@@ -36,9 +36,9 @@
                 <div class="white-text"><?php the_sub_field('descricao_da_secao_doar'); ?></div>
                 <?php if (get_sub_field('links_de_doacao')): 
                     $values = get_sub_field('links_de_doacao'); 
-                        if($values) : foreach($values as $value): print_r($value['titulo_botao'])?>
-                <p><a class="waves-effect waves-light btn" href="<#?= $value->link_do_botao; ?>" target="_blank" rel="noopener noreferrer">
-                <i class="material-icons left">attach_money</i><#?= $value->titulo_botao; ?></a></p>
+                        if($values) : foreach($values as $value): ?>
+                <p><a class="waves-effect waves-light btn" href="<?= $value['link_do_botao']; ?>" target="_blank" rel="noopener noreferrer">
+                <i class="material-icons left">attach_money</i><?= $value['titulo_botao']; ?></a></p>
                 <?php endforeach; endif; endif; ?>
             </div>
         </div>
